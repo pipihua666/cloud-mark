7610d5615f0cb4c856b86e052133c2d64c3409ab
zhaokh <zhaokh@knownsec.com>
2021-07-15 23:17:05 +0800

    添加markdown编辑器，优化界面

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
+/*
+ * @Author: pipihua
+ * @Date: 2021-07-08 22:42:48
+ * @LastEditTime: 2021-07-15 22:47:23
+ * @LastEditors: pipihua
+ * @Description: electron主进程
+ * @FilePath: /cloud-mark/main.js
+ * 佛祖保佑永无BUG
+ */
 const { app, BrowserWindow, globalShortcut } = require('electron')
 const isDev = require('electron-is-dev')
 
 app.on('ready', () => {
   const mainWindow = new BrowserWindow({
-    height: 1000,
-    with: 1200,
+    width: 1440,
+    height: 768,
     webPreferences: {
       nodeIntegration: true
     }
--- src/App.js
+++ src/App.js
@@ -1,16 +1,63 @@
+/*
+ * @Author: pipihua
+ * @Date: 2021-07-08 22:40:53
+ * @LastEditTime: 2021-07-15 23:15:28
+ * @LastEditors: pipihua
+ * @Description: 主应用
+ * @FilePath: /cloud-mark/src/App.js
+ * 佛祖保佑永无BUG
+ */
+import { useState } from 'react'
 import FileHeader from './components/FileSearch'
 import FileList from './components/FileList'
 import mocks from './fileMock'
 import ButtonBtn from './components/ButtomBtn'
 import TabList from './components/TabList'
 import { faPlus, faFileImport } from '@fortawesome/free-solid-svg-icons'
+import SimpleMDE from 'react-simplemde-editor'
+import 'easymde/dist/easymde.min.css'
 
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
-  const onTabClick = () => {}
-  const onTabClose = () => {}
+  const [files, setFiles] = useState({})
+  const [activeFileID, setActiveFileID] = useState('')
+  const [openedFileIDs, setOpenedFileIDs] = useState([])
+  const [unsavedFileIDs, setUnsavedFileIDs] = useState([])
+  const activeFile = files[activeFileID]
+  const openedFiles = openedFileIDs.map(openID => {
+    return files[openID]
+  })
+
+  const fileChange = (id, value) => {
+    if (value !== files[id].body) {
+      const newFile = { ...files[id], body: value }
+      setFiles({ ...files, [id]: newFile })
+      // update unsavedIDs
+      if (!unsavedFileIDs.includes(id)) {
+        setUnsavedFileIDs([...unsavedFileIDs, id])
+      }
+    }
+  }
+
+  const tabClick = fileID => {
+    // set current active file
+    setActiveFileID(fileID)
+  }
+
+  const tabClose = id => {
+    //remove current id from openedFileIDs
+    const tabsWithout = openedFileIDs.filter(fileID => fileID !== id)
+    setOpenedFileIDs(tabsWithout)
+    // set the active to the first opened tab if still tabs left
+    if (tabsWithout.length > 0) {
+      setActiveFileID(tabsWithout[0])
+    } else {
+      setActiveFileID('')
+    }
+  }
+
   return (
     <div className="App container-fluid px-0">
       <div className="row">
@@ -28,7 +75,7 @@ function App() {
               console.log('file delete:', id)
             }}
           />
-          <div className="d-grid gap-2 d-md-flex justify-content-center mt-2">
+          <div className="d-grid gap-2 d-md-flex justify-content-center mt-2 button-group">
             <ButtonBtn icon={faPlus} text="新建" colorClass="btn-primary" />
             <ButtonBtn
               icon={faFileImport}
@@ -38,13 +85,30 @@ function App() {
           </div>
         </div>
         <div className="col-7 right-panel">
-          <TabList
-            files={[]}
-            activeId=""
-            unsaveIds={[]}
-            onTabClick={onTabClick}
-            onCloseTab={onTabClose}
-          />
+          {!activeFile && (
+            <div className="start-page">选择或者创建新的 Markdown 文档</div>
+          )}
+          {activeFile && (
+            <>
+              <TabList
+                files={openedFiles}
+                activeId={activeFileID}
+                unsaveIds={unsavedFileIDs}
+                onTabClick={tabClick}
+                onCloseTab={tabClose}
+              />
+              <SimpleMDE
+                key={activeFile && activeFile.id}
+                value={activeFile && activeFile.body}
+                onChange={value => {
+                  fileChange(activeFile.id, value)
+                }}
+                options={{
+                  minHeight: '515px'
+                }}
+              />
+            </>
+          )}
         </div>
       </div>
     </div>
